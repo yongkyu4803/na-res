5edf35f94182e20c0aae6b9ca9d4a37a036e379b
SYPark <parkyongkyu0@gmail..com>
2025-02-18 15:29:41 +0900

    UI: Fix table header alignment and improve table styles

--- app.py
+++ app.py
@@ -154,8 +154,8 @@ if not df.empty:
             min-width: 100px;
         }
         .custom-table td:nth-child(4), .custom-table th:nth-child(4) {
-            width: 25%;  /* 세 번째 열 */
-            min-width: 100px;
+            width: 20%;  /* 세 번째 열 */
+            min-width: 80px;
         }
         /* 특정 열의 폰트 크기 조정 */
         .custom-table td:nth-child(1), .custom-table th:nth-child(1) {
